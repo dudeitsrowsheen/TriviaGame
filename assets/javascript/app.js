4cc9dd743718454182ca037ff39421da305e9aa6
Roisin Hayes Owens <roisinhayesowens@gmail.com>
2019-05-13 21:49:39 -0500

    torublshootingbutton

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,55 +9,71 @@ $(document).ready(function () {
         timeRemaining: 120,
 
         startTimer: function () {
-            $("#timer").text("You got this! 2:00 mins!" + gameStatus.timeRemaining);
+            $("#timer").text("Time remaining: " + gameStatus.timeRemaining);
             setInterval(gameStatus.countdown, 1000);
             $("#firstPage").hide();
-            trivia.displayQuestions();
-            console.log(gameStatus.startTimer)
+            questions.displayQuestions();
+            // console.log(gameStatus.startTimer)
         },
 
 
         countdown: function () {
             gameStatus.timeRemaining--;
-            $("#timer").text("You got this! 2:00 mins!" + gameState.timeRemaining);
+            $("#timer").text("Time remaining: " + gameStatus.timeRemaining);
             if (gameStatus.timeRemaining === 0) {
                 gameStatus.stopTimer();
                 $("#timer").empty();
-                console.log(countdown);
+                // console.log(countdown);
             }
-
-
         },
+
         stopTimer: function () {
             clearInterval();
-            trivia.checkAnswers();
+            questions.checkAnswers();
         },
 
         // $("#timer").html("<h2>" + number + "</h2>");
 
 
-        // showEndPage: function(numCorrect, numIncorrect, numUnanswered) {
-        //     $("#secondPage").show();
-        //     $("#questions").empty();
-        //     $("#timer").empty();
-        //     $("#timer").hide();
-        //     $("#correct").text("correct (YASSS QWEEN!): " + numCorrect);
-        //     $("#incorrect").text("incorrect (BITCH WHAT?): " + numIncorrect);
-        //     $("#unanswered").text("unanswered (I mean you tried..): " + numUnanswered);
-        //   }
+        showEndPage: function (numCorrect, numIncorrect, numUnanswered) {
+            $("#secondPage").show();
+            $("#questions").empty();
+            $("#timer").empty();
+            $("#timer").hide();
+            $("#correct").text("correct (YASSS QWEEN!): " + numCorrect);
+            $("#incorrect").text("incorrect (BITCH WHAT?): " + numIncorrect);
+            $("#unanswered").text("unanswered (I mean you tried..): " + numUnanswered);
+        }
+    }
 
+    var questions = {
 
+        displayQuestions: function () {
+            var divContainer = $("#questions");
+            var answerGroup = $(".form-check");
+            divContainer.append('<h2>How well do you know Broad City?</h2>');
 
-        // $(document)on.("click",); 
+            for (var i = 0; i < questionList.length; i++) {
 
-        // $(document).ready(function () {
-        // click: function(event)
-        //         $("#start-button").hide();
-        //         startTrivia();
-        //         timer.start();
-        //         $(".done").show();
-        //     },
+                divContainer.append('<div id="question">' + questionBank[i].question + '</div>');
+
+                var answer1 = questionBank[i].answers[0];
+                var answer2 = questionBank[i].answers[1];
+                var answer3 = questionBank[i].answers[2];
+
+            }
+
+        }
     }
 
 });
 
+ // $(document)on.("click",); 
+
+    // $(document).ready(function () {
+    // click: function(event)
+    //         $("#start-button").hide();
+    //         startTrivia();
+    //         timer.start();
+    //         $(".done").show();
+    //     }
