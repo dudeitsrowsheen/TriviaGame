791c1ea7a57e155cdc4c5dbfe905f7d71bf4c501
Roisin Hayes Owens <roisinhayesowens@gmail.com>
2019-05-13 19:49:28 -0500

    adding css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,17 @@
-$(document).ready(function(){
-        $("h2").click(function(){
-          $(this).hide();
-        }
-        );
-
-    //     event += parseInt($(this).attr('value'));
-
-    // });
+$(document).ready(function () {
+    $("h2").click(function () {
+        $(this).hide();
+    }
+    );
 
     var gameStatus = {
 
         timeRemaining: 120,
 
         startTimer: function () {
-            $("#timer").text("Hurry! " + gameStatus.timeRemaining);
+            $("#timer").text("You got this! 2:00 mins!" + gameStatus.timeRemaining);
             setInterval(gameStatus.countdown, 1000);
-            $("#homePage").hide();
+            $("#firstPage").hide();
             trivia.displayQuestions();
             console.log(gameStatus.startTimer)
         },
@@ -23,16 +19,37 @@ $(document).ready(function(){
 
         countdown: function () {
             gameStatus.timeRemaining--;
-            $("#timer").text("Time remaining: " + gameState.timeRemaining);
+            $("#timer").text("You got this! 2:00 mins!" + gameState.timeRemaining);
             if (gameStatus.timeRemaining === 0) {
-                gameState.stopTimer();
+                gameStatus.stopTimer();
                 $("#timer").empty();
                 console.log(countdown);
             }
+
+
+        },
+        stopTimer: function () {
+            clearInterval();
+            trivia.checkAnswers();
         },
 
+        // $("#timer").html("<h2>" + number + "</h2>");
+
+
+        // showEndPage: function(numCorrect, numIncorrect, numUnanswered) {
+        //     $("#secondPage").show();
+        //     $("#questions").empty();
+        //     $("#timer").empty();
+        //     $("#timer").hide();
+        //     $("#correct").text("correct (YASSS QWEEN!): " + numCorrect);
+        //     $("#incorrect").text("incorrect (BITCH WHAT?): " + numIncorrect);
+        //     $("#unanswered").text("unanswered (I mean you tried..): " + numUnanswered);
+        //   }
+
+
+
         // $(document)on.("click",); 
-        
+
         // $(document).ready(function () {
         // click: function(event)
         //         $("#start-button").hide();
@@ -41,6 +58,6 @@ $(document).ready(function(){
         //         $(".done").show();
         //     },
     }
-    
-    });
-    
+
+});
+
