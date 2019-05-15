2290d8d5d63d1f53d9e65cedf00a6c167a82d6a6
Roisin Hayes Owens <roisinhayesowens@gmail.com>
2019-05-14 20:38:17 -0500

    timer displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,31 @@
 $(document).ready(function () {
     $("h2").click(function () {
         $(this).hide();
-    }
-    );
+        // $("#start-button").on("click", gameStatus.startTimer);
+        gameStatus.startTimer();
+    });
 
     var gameStatus = {
 
-        timeRemaining: 120,
+        timeRemaining: 3,
 
         startTimer: function () {
             $("#timer").text("Time remaining: " + gameStatus.timeRemaining);
             setInterval(gameStatus.countdown, 1000);
-            $("#firstPage").hide();
-            questions.displayQuestions();
-            // console.log(gameStatus.startTimer)
+            $("#first-Page").hide();
+            trivia.displayQuestions();
+            console.error(gameStatus.timer);
+            console.log("timer started");
         },
 
 
         countdown: function () {
             gameStatus.timeRemaining--;
             $("#timer").text("Time remaining: " + gameStatus.timeRemaining);
-            if (gameStatus.timeRemaining === 0) {
+            if (gameStatus.timeRemaining === 0); {
                 gameStatus.stopTimer();
-                $("#timer").empty();
-                // console.log(countdown);
+                // countdown = Math.max(1, countdown);
+                $(timer).empty();
             }
         },
 
@@ -31,49 +33,37 @@ $(document).ready(function () {
             clearInterval();
             questions.checkAnswers();
         },
-
-        // $("#timer").html("<h2>" + number + "</h2>");
-
-
-        showEndPage: function (numCorrect, numIncorrect, numUnanswered) {
-            $("#secondPage").show();
+        displayAnswerPage: function (correct, incorrect, unanswered) {
+            $("#answerPage").display();
             $("#questions").empty();
             $("#timer").empty();
-            $("#timer").hide();
-            $("#correct").text("correct (YASSS QWEEN!): " + numCorrect);
-            $("#incorrect").text("incorrect (BITCH WHAT?): " + numIncorrect);
-            $("#unanswered").text("unanswered (I mean you tried..): " + numUnanswered);
+            $("#yes").text("YASSS QWEEN!" + correct);
+            $("#no").text("EXCUSE-AH-ME?" + incorrect);
+            $("#unanswered").text("You don't watch Broad City" + unanswered);
         }
     }
+    console.log(countdown);s
+    console.log(gameStatus.timeRemaining);
 
-    var questions = {
+
+    var game = {
 
         displayQuestions: function () {
-            var divContainer = $("#questions");
-            var answerGroup = $(".form-check");
-            divContainer.append('<h2>How well do you know Broad City?</h2>');
+            var container = $("#questions");
+            var answerList = $(".check-answers");
+            container.append('<h2>Lets go baby!</h2>');
 
             for (var i = 0; i < questionList.length; i++) {
 
-                divContainer.append('<div id="question">' + questionBank[i].question + '</div>');
+                container.append('<div id="question">' + questionList[i].question + '</div>');
 
-                var answer1 = questionBank[i].answers[0];
-                var answer2 = questionBank[i].answers[1];
-                var answer3 = questionBank[i].answers[2];
+                var answer1 = questionList[i].answers[0];
+                var answer2 = questionList[i].answers[1];
+                var answer3 = questionList[i].answeers[2];
+
+
 
             }
-
         }
     }
-
-});
-
- // $(document)on.("click",); 
-
-    // $(document).ready(function () {
-    // click: function(event)
-    //         $("#start-button").hide();
-    //         startTrivia();
-    //         timer.start();
-    //         $(".done").show();
-    //     }
+})
